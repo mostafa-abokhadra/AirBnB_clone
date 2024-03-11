a481c579c021a1cb228f16f90a5ad5fe01299ab4
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-11 13:49:11 +0200

    uni tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,10 +1,46 @@
 #!/usr/bin/python3
-"""testing base class"""
 
-from models.base_model import BaseModel
+"""The unittest for the BaseModel Class"""
+
 import unittest
+from models.base_model import BaseModel
 
-class test_base_(unittest.TestCase):
 
-if __name__ == "__main__":
-    unittest.main()
+class TestBaseModel(unittest.TestCase):
+    """The start of unittest for BaseModel Class"""
+
+    def setUp(self):
+        """setint up an instance of BaseModel"""
+        self.bm = BaseModel()
+
+    def test_new_instance(self):
+        """tests the values of an intanct of BaseModel"""
+        self.assertIsInstance(self.bm, BaseModel)
+        self.assertIsNotNone(self.bm.id)
+        self.assertIsNotNone(self.bm.created_at)
+        self.assertIsNotNone(self.bm.updated_at)
+
+    def test_to_dict_method(self):
+        """test to_dict method"""
+        bm_dict = self.bm.to_dict()
+        self.assertEqual(bm_dict["__class__"], "BaseModel")
+        self.assertTrue("__class__" in bm_dict)
+        self.assertTrue("created_at" in bm_dict)
+        self.assertTrue("updated_at" in bm_dict)
+        self.assertTrue("id" in bm_dict)
+
+    def test_str_method(self):
+        """test str method"""
+        self.assertEqual(str(self.bm),
+                         f'[BaseModel] ({self.bm.id}) {self.bm.__dict__}')
+
+    def test_save_method(self):
+        """test self.update_at before and after the save method"""
+        initial_updated_at = self.bm.updated_at
+        self.bm.save()
+        self.assertNotEqual(initial_updated_at, self.bm.updated_at)
+
+    def test_to_dict_method_with_kwargs(self):
+        """test to_dict method with kwargs"""
+        bm_dict = self.bm.to_dict()
+        self.assertEqual(bm_dict["__class__"], "BaseModel")
